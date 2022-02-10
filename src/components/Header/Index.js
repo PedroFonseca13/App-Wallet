cc6299f591e61c9072ca68c5ffbcc41ac2ad3823
Pedro Fonseca <pedrinhopedro01@gmail.com>
2022-02-10 16:45:39 -0300

    requirement 02 & 03 - ✅

--- src/components/Header/Index.js
+++ src/components/Header/Index.js
@@ -1,18 +1,43 @@
+import PropTypes from 'prop-types';
 import React, { Component } from 'react';
 import { FaUserAlt } from 'react-icons/fa';
+import { MdAttachMoney } from 'react-icons/md';
+import { connect } from 'react-redux';
 import './style.css';
 
-export default class Header extends Component {
+class Header extends Component {
   render() {
+    const { email } = this.props;
+
     return (
       <header>
         <div>
           <h3>TrybeWallet</h3>
         </div>
         <div>
-          <FaUserAlt />
+          <div data-testid="email-field">
+            <FaUserAlt />
+            {' '}
+            {email}
+          </div>
+          <div data-testid="total-field">
+            <MdAttachMoney />
+            {' '}
+            0
+            <span data-testid="header-currency-field">{' BRL'}</span>
+          </div>
         </div>
       </header>
     );
   }
 }
+
+Header.propTypes = {
+  email: PropTypes.any,
+}.isRequired;
+
+const mapStateToProps = ({ user }) => ({
+  email: user.email,
+});
+
+export default connect(mapStateToProps)(Header);
